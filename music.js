eecd0d1857e1cf961d4910bb2746e843de8af84b
jayumaksgit config --global user.name jayumaksgit config --global user.name jayumaks <jayumaks@gmail.com>
2022-02-10 05:42:51 +0100

    latest

--- music.js
+++ music.js
@@ -12,7 +12,7 @@ const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
 // Song titles
-const songs = ['hey', 'summer', 'ukulele'];
+const songs = ['1', '2', '3'];
 
 // Keep track of song
 let songIndex = 2;
